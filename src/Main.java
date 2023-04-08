196b68bc4ae599cfe0392b3590394a05c8088cfd
SIDORYCH <vladimiriv@hotmail.com>
2023-04-08 09:55:28 +0300

    Билет №6, задача на перехват и обработку исключения

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+/**
+ * Билет №6:
+ * задача - Напишите пример перехвата и обработки исключения в блоке try-catch метода
+ *
+ * пример перехвата исключений ArithmeticException и ArrayIndexOutOfBoundsException
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] arr = {1, 2, 0, 4};
+
+        try {
+            System.out.println("Начало работы");
+
+ //           System.out.println((arr[1] / arr[2]));
+            arr[5] = 8;
+            for (int num : arr) {
+                System.out.println(num);
+            }
+
+        } catch (ArithmeticException e) {
+            System.err.println(e + " ----  Операция деления на ноль !");
+        } catch (ArrayIndexOutOfBoundsException e1) {
+            System.err.println(e1 + " ----  Выход за пределы длины массива !");
+        }
+        finally {
+            System.out.println("завершение работы");
+        }
     }
 }
+
